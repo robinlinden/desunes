0594ef69a7b4ead97bc9a229376c7d1a7df5f8f2
Robin Linden <dev@robinlinden.eu>
2020-01-02 00:47:24 +0100

    Use SFML to display a window

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,3 +1,16 @@
+#include <SFML/Window.hpp>
+
 int main(int argc, char **argv) {
+    sf::Window window(sf::VideoMode(640, 480), "desunes");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+
     return 0;
 }
