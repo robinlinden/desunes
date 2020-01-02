6dc2b21bd93ba587dd4f9bf74c6320d5ad54a701
Robin Linden <dev@robinlinden.eu>
2020-01-03 00:09:34 +0100

    Use imgui to display some example GUI components

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,16 +1,38 @@
-#include <SFML/Window.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
 int main(int argc, char **argv) {
-    sf::Window window(sf::VideoMode(640, 480), "desunes");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "desunes");
+    ImGui::SFML::Init(window);
+
+    sf::Clock deltaClock;
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("window");
+        ImGui::Text("hello");
+        ImGui::Button("button");
+        ImGui::End();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
     }
+
+    ImGui::SFML::Shutdown();
 
     return 0;
 }
